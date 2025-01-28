8e5210e8dd6c9c4c9aaf02a9fb9ca17da640f08d
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-01-28 12:50:43 -0800

    Doing the Css for the Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import "./Weather.css";
+
 export default function Weather() {
   const [city, setCity] = useState("");
   const [valueDefault, setValueDefault] = useState("");
@@ -8,44 +10,61 @@ export default function Weather() {
   function displayWeather(response) {
     console.log(response.data);
   }
+  function changeCity(event) {
+    setValueDefault(event.target.value);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
     setCity(valueDefault);
-    let apiKey = "c4249306fe36f002cd05252621ef3fa6";
-    let api = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+    let apiKey = "eac360db5fc86ft86450f3693e73o43f";
+    let api = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(api).then(displayWeather);
   }
-  function changeCity(event) {
-    setValueDefault(event.target.value);
-  }
+
   return (
-    <div>
-      <img
-        src="https://www.shecodes.io/assets/branding/logo-shecodes-3dfa60aeab8ef361842da5a2b6d46db3af1b7afafefee3dde0a9846389de754b.png"
-        alt="Logo"
-      />
-      <form onSubmit={handleSubmit}>
-        <input type="search" value={valueDefault} onChange={changeCity} />
-        <input type="submit" value="Search" />
-      </form>
-      <div>
-        <h3>{city}</h3>
-        <p>Wednesday</p>
-        <p>10:01pm</p>
-        <p>Clouds</p>
-        <p>Humidity: 68%</p>
-        <p>Wind: 4.02km/h</p>
-      </div>
-      <div>
-        <p>☀️</p>
-        <h2>14°C</h2>
-      </div>
-      <div>
-        <p>Fri</p>
-        <p>🌤️</p>
-        <p>14° 10°</p>
-      </div>
+    <div className="Weather">
+      <header>
+        <img
+          src="https://www.shecodes.io/assets/branding/logo-shecodes-3dfa60aeab8ef361842da5a2b6d46db3af1b7afafefee3dde0a9846389de754b.png"
+          alt="Logo"
+        />
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            value={valueDefault}
+            onChange={changeCity}
+            className="SearchBar"
+          />
+          <input type="submit" value="Search" />
+        </form>
+      </header>
+      <main>
+        <div>
+          <h3>{city}</h3>
+          <p>Wednesday</p>
+          <p>10:01pm</p>
+          <p>Clouds</p>
+          <p>Humidity: 68%</p>
+          <p>Wind: 4.02km/h</p>
+        </div>
+        <div>
+          <p>☀️</p>
+          <h2>14°C</h2>
+        </div>
+        <div>
+          <p>Fri</p>
+          <p>🌤️</p>
+          <p>14° 10°</p>
+        </div>
+      </main>
+      <footer>
+        <p>
+          This project was coded by SheCodes and is open-sourced on GitHub and
+          hosted on Netlify
+        </p>
+      </footer>
     </div>
   );
 }
